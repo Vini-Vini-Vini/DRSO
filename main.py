3ac6b6494c8554c3e4d1e9a8765cbb3cc2c96630
Vini-Vini-Vini <waseda3979@akanw.waseda.jp>
2026-07-05 15:17:27 +0900

    main.pyのデータパスを絶対パスに修正

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ print("----------------------------------------")
 
 ### 1. load and convert statsbomb data ###
 if args.data == "statsbomb":
-    data_folder = "open-data-20231002/data"
+    data_folder = "/home/furujun/pass/open-data-20231002/data"
     DLoader = StatsBombLoader(root=data_folder, getter="local")
 else:
     pdb.set_trace()
