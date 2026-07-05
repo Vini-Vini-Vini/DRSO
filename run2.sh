5ded2138d29f1930facf828cebaab77fb10ec6c6
Vini-Vini-Vini <waseda3979@akanw.waseda.jp>
2026-07-05 18:30:21 +0900

    並列実行スクリプトの修正(並列数調整、Ctrl+C対応)

--- run2.sh
+++ run2.sh
@@ -1,6 +1,6 @@
 ### 2. calculate PPCF and OBSO ###
-num_runs=1
-max_parallel=2
-command="CUDA_VISIBLE_DEVICES="5" python main.py --data statsbomb --game wc2022 --set_vel 5.0 --skip_load_rawdata --skip_compare_the_number_of_players --skip_verify_obso --skip_identify_optimal_positioning --skip_evaluate_team_defense --skip_show_results"
+num_runs=64
+max_parallel=16
+command="CUDA_VISIBLE_DEVICES=5,7 python main.py --data statsbomb --game wc2022 --set_vel 5.0 --skip_load_rawdata --skip_compare_the_number_of_players --skip_verify_obso --skip_identify_optimal_positioning --skip_evaluate_team_defense --skip_show_results"
 seq $num_runs | xargs -I {} -P $max_parallel sh -c "echo '[$(date)] Running command {}'; $command --count {}"
 wait
