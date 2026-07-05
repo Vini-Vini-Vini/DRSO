c670eabfba79744ab74cb31868210d46137116cb
Vini-Vini-Vini <waseda3979@akanw.waseda.jp>
2026-07-05 13:00:21 +0900

    enigma03でデータのäタの保存先

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ print("----------------------------------------")
 
 ### 1. load and convert statsbomb data ###
 if args.data == "statsbomb":
-    data_folder = "/home/furujun/work"
+    data_folder = "open-data-20231002/data"
     DLoader = StatsBombLoader(root=data_folder, getter="local")
 else:
     pdb.set_trace()
